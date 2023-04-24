c49445fe75e2015db33cc1d7f9952a8de8ea4562
yul <xiangyulong@microconnect.com>
2023-04-24 11:00:01 +0800

    test: 新增h5测试

--- jest.config.js
+++ jest.config.js
@@ -5,7 +5,6 @@ module.exports = {
         compile: true,
         h5: {
             // 为了节省测试时间，可以指定一个 H5 的 url 地址，若不指定，每次运行测试，会先 npm run dev:h5
-            url: 'http://192.168.x.x:8080/h5/',
             options: {
                 headless: false, // 配置是否显示 puppeteer 测试窗口
             },
